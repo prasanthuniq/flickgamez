3bb8a80cfbc59523c5322e7c20a4c2a63d3306ca
prasanthuniq <62007879+prasanthuniq@users.noreply.github.com>
2020-03-10 13:30:52 +0530

    Add files via upload

---
+++ js/dynamic.js
@@ -0,0 +1,64 @@
+<script type="text/javascript">
+
+	function dcontent(v)
+	{
+	if(v==1)
+	{
+	document.getElementById('c').style.display="none";
+	document.getElementById('c1').style.display="block";
+	document.getElementById('c2').style.display="none";
+	document.getElementById('c3').style.display="none";
+	document.getElementById('c4').style.display="none";document.getElementById('c5').style.display="none";
+	document.getElementById('mimg').src="3.jpg"
+	}
+	else if(v==2)
+	{
+	document.getElementById('c').style.display="none";
+	document.getElementById('c1').style.display="none";
+	document.getElementById('c2').style.display="block";
+	document.getElementById('c3').style.display="none";
+	document.getElementById('c4').style.display="none";document.getElementById('c5').style.display="none";
+	document.getElementById('mimg').src="4.jpg"
+	}
+	else if(v==3)
+	{
+	document.getElementById('c').style.display="none";
+	document.getElementById('c1').style.display="none";
+	document.getElementById('c2').style.display="none";
+	document.getElementById('c3').style.display="block";
+	document.getElementById('c4').style.display="none";document.getElementById('c5').style.display="none";
+	document.getElementById('mimg').src="5.jpg"
+	}
+	
+	else if(v==4)
+	{
+	document.getElementById('c').style.display="none";
+	document.getElementById('c1').style.display="none";
+	document.getElementById('c2').style.display="none";
+	document.getElementById('c3').style.display="none";
+	document.getElementById('c4').style.display="none";document.getElementById('c5').style.display="block";
+	document.getElementById('mimg').src="6.jpg"
+	}
+	else if(v==5)
+	{
+	document.getElementById('c').style.display="none";
+	document.getElementById('c1').style.display="none";
+	document.getElementById('c2').style.display="none";
+	document.getElementById('c3').style.display="none";
+	document.getElementById('c4').style.display="none";
+	document.getElementById('c5').style.display="block";
+	document.getElementById('mimg').src="7.jpg"
+	}
+	else if(v==6)
+	{
+	document.getElementById('c').style.display="block";
+	document.getElementById('c1').style.display="none";
+	document.getElementById('c2').style.display="none";
+	document.getElementById('c3').style.display="none";
+	document.getElementById('c4').style.display="none";
+	document.getElementById('c5').style.display="none";
+	document.getElementById('mimg').src="2.jpg"
+	}
+	
+	}
+</script>
